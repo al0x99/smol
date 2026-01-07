0d505e97b80d33291e1bda1f69a743a9381e4912
Alin <41188223+al0x99@users.noreply.github.com>
2026-01-08 00:08:14 +0100

    Initial Commit

---
+++ smol/smolApp.swift
@@ -0,0 +1,32 @@
+//
+//  smolApp.swift
+//  smol
+//
+//  Created by Alin Sfirschi on 08/01/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct smolApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ smolTests/smolTests.swift
@@ -0,0 +1,17 @@
+//
+//  smolTests.swift
+//  smolTests
+//
+//  Created by Alin Sfirschi on 08/01/26.
+//
+
+import Testing
+@testable import smol
+
+struct smolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
